f11d61917fbf992ba8ad53942c160e2f8f65ec8d
tkostadinov004 <tkostadinov04@gmail.com>
2022-08-08 15:06:14 +0300

    Started adding ideology icons

---
+++ js/axes.js
@@ -0,0 +1,248 @@
+axes = [
+    {
+        "id": 0,
+        "shortenedName": "economy",
+        "leftSide": "socialist",
+        "rightSide": "capitalist",
+        "labels": ["Радикален комунист", "Комунист", "Социалист", "Социалдемократ", "Регулиран капитализъм", "Капитализъм", "Олигархия", "Социален дарвинизъм"]
+    },
+    {
+        "id": 1,
+        "shortenedName": "auth",
+        "leftSide": "libertarian",
+        "rightSide": "authoritarian",
+        "labels": ["Егоист", "Анархист", "Либертарианец", "Либерал", "Етатист", "Авторитарен", "Тоталитарен", "АНГСОЦ"]
+    },
+    {
+        "id": 2,
+        "shortenedName": "nat",
+        "leftSide": "globalist",
+        "rightSide": "nationalist",
+        "labels": ["Световно правителство", "Глобалист", "Интернационалист", "Дипломатичен", "Патриот", "Националист", "Шовинист", "Империалист"]
+    },
+    {
+        "id": 3,
+        "shortenedName": "trad",
+        "leftSide": "progressive",
+        "rightSide": "conservative",
+        "labels": ["Новелтизъм", "Революционен", "Прогресивен", "Реформист", "Консервативен", "Традиционалист", "Реакционер", "Антиквист"]
+    },
+    {
+        "id": 4,
+        "shortenedName": "acc",
+        "leftSide": "decelerationist",
+        "rightSide": "accelerationist",
+        "labels": ["Прималист", "Примитивист", "Децелерационист", "Анти-индустриален", "Продуктивист", "Акселерационист", "Трансхуманист", "Постхуманист"]
+    },
+    {
+        "id": 5,
+        "shortenedName": "religion",
+        "leftSide": "atheist",
+        "rightSide": "religious",
+        "labels": ["Анти-религия", "Атеист", "Агностик", "Скептик", "Вярващ", "Религиозен", "Теократичен", "Радикален теист"]
+    },
+    {
+        "id": 6,
+        "shortenedName": "nonv",
+        "leftSide": "violent",
+        "rightSide": "nonviolent",
+        "labels": ["Терорист", "Агресивен", "Враждебно настроен", "Териториален", "Неагресивен", "Ненасилствен", "Мирен", "Пацифист"]
+    },
+    {
+        "id": 7,
+        "shortenedName": "aristoc",
+        "leftSide": "democrat",
+        "rightSide": "aristocrat",
+        "labels": ["Управление на произволно избраните", "Какистократ", "Охлократ", "Демократ", "Аристократ", "Ноократ", "Технократ", "Киберократ"]
+    },
+    {
+        "id": 8,
+        "shortenedName": "const",
+        "leftSide": "repdem",
+        "rightSide": "absolute",
+        "labels": ["Невронна мрежа", "Директна демокрация", "Представителна демокрация", "Изборна демокрация", "Конституционен", "Полуконституционен", "Абсолютна власт", "Една мисъл"]
+    },
+    {
+        "id": 9,
+        "shortenedName": "inter",
+        "leftSide": "isolationist",
+        "rightSide": "interventionist",
+        "labels": ["Хиперизолационист", "Изолационист", "Твърд релативист", "Релативист", "Кооперативен", "Интервенционалист", "Универсалист", "Експанзионист"]
+    },
+    {
+        "id": 11,
+        "shortenedName": "culturalRel",
+        "leftSide": "multicultural",
+        "rightSide": "monocultural",
+        "labels": ["Културна анархия", "Далтонист", "Мултикултурен", "Поликултурен", "Монокултурен", "Асимилация", "Сегрегация", "Един етнос, една държава"]
+    },
+    {
+        "id": 12,
+        "shortenedName": "prag",
+        "leftSide": "idealist",
+        "rightSide": "realist",
+        "labels": ["Утопичен", "Кихотичен", "Идеалист", "Оптимист", "Реалист", "Прагматичен", "Рационалист", "Анти-утопичен"]
+    },
+    {
+        "id": 13,
+        "shortenedName": "prud",
+        "leftSide": "perslib",
+        "rightSide": "moralism",
+        "labels": ["Морален егоизъм", "Хедонизъм", "Упадък", "Лична свобода", "Морализъм", "Прудизъм", "Твърдо морален", "Морален дарвинизъм"]
+    },
+    {
+        "id": 14,
+        "shortenedName": "free",
+        "leftSide": "regulationism",
+        "rightSide": "freemarket",
+        "labels": ["Анти-икономически", "Корпоративизъм", "Планова икономика", "Икономически регулации", "Смесена икономика", "Свободен пазар", "Лесефер", "Икономическа евгеника"]
+    },
+    {
+        "id": 15,
+        "shortenedName": "pat",
+        "leftSide": "youthrights",
+        "rightSide": "paternalism",
+        "labels": ["Бебешка тирания", "Педиархия", "Младежки активизъм", "Детски труд", "Възрастова дискриминация", "Патернализъм", "Геронтокрация", "Некрокрация"]
+    },
+    {
+        "id": 16,
+        "shortenedName": "unru",
+        "leftSide": "organized",
+        "rightSide": "disorderly",
+        "labels": ["Абсолютист", "Спазващ закона", "Дисциплиниран", "Организиран", "Неорганизиран", "Непокорен", "Хаотичен", "Луд"]
+    },
+    {
+        "id": 17,
+        "shortenedName": "imp",
+        "leftSide": "altruistic",
+        "rightSide": "rude",
+        "labels": ["Безкористен", "Добър", "Алтруист", "Нормален", "Груб", "Нечист", "Зъл", "Подъл"]
+    },
+    {
+        "id": 18,
+        "shortenedName": "federal",
+        "leftSide": "centralism",
+        "rightSide": "decentralism",
+        "labels": ["Безкористен", "Добър", "Алтруист", "Нормален", "Груб", "Нечист", "Зъл", "Подъл"]
+    },
+    {
+        "id": 19,
+        "shortenedName": "nih",
+        "leftSide": "collectivism",
+        "rightSide": "individualism"
+    },
+    {
+        "id": 20,
+        "shortenedName": "gadsden",
+        "leftSide": "surveillance",
+        "rightSide": "privacy"
+    },
+    {
+        "id": 21,
+        "shortenedName": "puni",
+        "leftSide": "rehab",
+        "rightSide": "punishment"
+    },
+    {
+        "id": 22,
+        "shortenedName": "spir",
+        "leftSide": "pragmatic",
+        "rightSide": "spiritualistic"
+    },
+    {
+        "id": 23,
+        "shortenedName": "lcen",
+        "leftSide": "past",
+        "rightSide": "future"
+    },
+    {
+        "id": 24,
+        "shortenedName": "pron",
+        "leftSide": "prochoice",
+        "rightSide": "prolife"
+    },
+    {
+        "id": 25,
+        "shortenedName": "clisk",
+        "leftSide": "environmentalism",
+        "rightSide": "industrialism"
+    },
+    {
+        "id": 26,
+        "shortenedName": "taxation",
+        "leftSide": "volunarytax",
+        "rightSide": "progressivetax"
+    },
+    {
+        "id": 27,
+        "shortenedName": "pmart",
+        "leftSide": "pastart",
+        "rightSide": "futureart"
+    },
+    {
+        "id": 28,
+        "shortenedName": "consq",
+        "leftSide": "deontological",
+        "rightSide": "conseq"
+    },
+    {
+        "id": 29,
+        "shortenedName": "enth",
+        "leftSide": "relaxed",
+        "rightSide": "productive"
+    },
+    {
+        "id": 30,
+        "shortenedName": "ahs",
+        "leftSide": "freespeech",
+        "rightSide": "censorship"
+    },
+    {
+        "id": 31,
+        "shortenedName": "fact",
+        "leftSide": "feelings",
+        "rightSide": "facts"
+    },
+    {
+        "id": 32,
+        "shortenedName": "trpop",
+        "leftSide": "educated",
+        "rightSide": "tribalpopulation"
+    },
+    {
+        "id": 33,
+        "shortenedName": "wmar",
+        "leftSide": "compulsoryweaponisation",
+        "rightSide": "gunban"
+    },
+    {
+        "id": 34,
+        "shortenedName": "trade",
+        "leftSide": "protectionism",
+        "rightSide": "freetrade"
+    },
+    {
+        "id": 35,
+        "shortenedName": "cmind",
+        "leftSide": "openminded",
+        "rightSide": "closeminded"
+    },
+    {
+        "id": 36,
+        "shortenedName": "poach",
+        "leftSide": "animalrights",
+        "rightSide": "poaching"
+    },
+    {
+        "id": 37,
+        "shortenedName": "hier",
+        "leftSide": "unionized",
+        "rightSide": "hierarchical"
+    },
+    {
+        "id": 38,
+        "shortenedName": "open",
+        "leftSide": "strictborders",
+        "rightSide": "openborders"
+    },
+];
---
+++ js/p53.js
@@ -0,0 +1,25 @@
+function getPixel(x, y) {
+                    let img;
+
+                    function preload() {
+                        img = loadImage('https://picsum.photos/200/300/?random');
+                    }
+
+                    function setup() {
+                        createCanvas(img.width, img.height);
+                    }
+
+                    function draw() {
+                        background(220);
+
+                        let pix = img.get(40, 60);
+console.log(1);
+                        image(img, 0, 0);
+
+                        // using functions
+                        console.log(`red: ${red(pix)}, green: ${green(pix)}, blue: ${blue(pix)}, alpha: ${alpha(pix)}`);
+                        // array
+                        console.log(`red: ${pix[0]}, green: ${pix[1]}, blue: ${pix[2]}, alpha: ${pix[3]}`);
+                    }
+
+                }
